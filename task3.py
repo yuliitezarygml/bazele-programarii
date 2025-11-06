ee8d22e67140f96793eaf9249ceffdecc178c142
Iulian <yuliitezarywork@gmail.com>
2025-11-06 08:58:32 +0200

    Delete task3.py

--- task3.py
+++
@@ -1,9 +0,0 @@
-def main():
-    N = int(input("Ион говорит число: "))
-
-    numbers = [N - 2, N - 1, N, N + 1, N + 2]
-
-    print("Василий говорит:", *numbers)
-
-if __name__ == "__main__":
-    main()
